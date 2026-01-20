78e269bd04369a30302cb7c644b09b4a060fba2f
Michael Medvedev <mmedvedev20@gmail.com>
2026-01-19 23:40:43 -0500

    install zustand and create canvas store

--- frontend/src/Canvas.tsx
+++ frontend/src/Canvas.tsx
@@ -1,47 +1,25 @@
-import { useState, useCallback } from 'react';
 import {
     Background,
     BackgroundVariant,
     Controls,
     ReactFlow,
-    addEdge,
-    applyEdgeChanges,
-    applyNodeChanges
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
+import useCanvasStore from './store';
 
 
-const initialNodes = [
-    { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-    { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
-];
-const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
 
 export default function Canvas() {
-    const [nodes, setNodes] = useState(initialNodes);
-    const [edges, setEdges] = useState(initialEdges);
-
-    const onNodesChange = useCallback(
-        (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
-        [],
-    );
-    const onEdgesChange = useCallback(
-        (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
-        [],
-    );
-    const onConnect = useCallback(
-        (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
-        [],
-    );
+    const store = useCanvasStore()
 
     return (
         <div style={{ width: '100%', height: '100%' }}>
             <ReactFlow
-                nodes={nodes}
-                edges={edges}
-                onNodesChange={onNodesChange}
-                onEdgesChange={onEdgesChange}
-                onConnect={onConnect}
+                nodes={store.nodes}
+                edges={store.edges}
+                onNodesChange={store.onNodesChange}
+                onEdgesChange={store.onEdgesChange}
+                onConnect={store.onConnect}
                 fitView
             >
                 <Controls />
---
+++ frontend/src/store.ts
@@ -0,0 +1,45 @@
+import { create } from 'zustand';
+import {
+    addEdge,
+    applyEdgeChanges,
+    applyNodeChanges,
+    type Connection,
+    type EdgeChange,
+    type NodeChange,
+    type Edge,
+    type Node,
+} from '@xyflow/react';
+
+const initialNodes: Node[] = [
+    { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+    { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+];
+const initialEdges: Edge[] = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+
+interface CanvasState {
+    nodes: Node[];
+    edges: Edge[];
+    onNodesChange: (changes: NodeChange[]) => void;
+    onEdgesChange: (changes: EdgeChange[]) => void;
+    onConnect: (params: Connection) => void;
+}
+
+
+const useCanvasStore = create<CanvasState>()((set) => ({
+    nodes: initialNodes,
+    edges: initialEdges,
+    onNodesChange: (changes) =>
+        set((state) => ({
+            nodes: applyNodeChanges(changes, state.nodes),
+        })),
+    onEdgesChange: (changes) =>
+        set((state) => ({
+            edges: applyEdgeChanges(changes, state.edges),
+        })),
+    onConnect: (params) =>
+        set((state) => ({
+            edges: addEdge(params, state.edges),
+        })),
+}));
+
+export default useCanvasStore;
